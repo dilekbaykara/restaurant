cda3701fce3dd7920d4b4eec4d0db2b923f53e84
Dilek Baykara <baykara.d@gmail.com>
2022-12-22 23:24:45 -0500

    added image to home page and styled borders

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,9 +1,12 @@
 export function home() {
+  var imgHome = document.createElement("img");
+  imgHome.src = "../images/main.png"
   var h1 = document.createElement("h1");
   var page = document.createElement("div");
 
   page.appendChild(h1);
   h1.innerText = "Brine - A Seafood Restaurant";
+  page.appendChild(imgHome);
 
   return page;
 }
