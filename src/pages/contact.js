ddee6bc62c8a9da6dab163a4498ebca859c05e92
Dilek Baykara <baykara.d@gmail.com>
2022-12-22 23:21:31 -0500

    adding information for Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,19 @@
 export function contact() {
+    var img = document.createElement("img");
+    img.src = "../images/map.png"
     var h1 = document.createElement("h1");
     var page = document.createElement("div");
-  
+    var h2 = document.createElement("h2");
+    var h3 = document.createElement("h3");
+
+   
     page.appendChild(h1);
+    page.appendChild(img);
     h1.innerText = "Brine - Contact";
+    page.appendChild(h2);
+    h2.innerText = "240 Central Park South New York, NY 10019"
+    page.appendChild(h3);
+    h3.innerText = "212-582-5100"
   
     return page;
   }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,10 +1,15 @@
 export function menu() {
     var h1 = document.createElement("h1");
+    var h2 = document.createElement("h2");
     var page = document.createElement("div");
-  
+
     page.appendChild(h1);
-    h1.innerText = "Brine - Menu";
+    page.appendChild(h1);
+    page.appendChild(h2);
   
+    h1.innerText = "Brine - Menu";
+    h1.innerText = "HAMACHI"
+    h2.innerText = "pacific yellowtail, meyer lemon, bergamot and basil aioli, pistachio"
     return page;
   }
   
