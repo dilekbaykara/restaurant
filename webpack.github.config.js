5508e35d91fcf64b413eb63e23a81f450614a90d
Dilek Baykara <baykara.d@gmail.com>
2023-01-16 17:22:40 -0500

    added docs folder

---
+++ webpack.github.config.js
@@ -0,0 +1,31 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  target: 'web',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'docs'),
+    publicPath:'/restaurant/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'
+    ],
+
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      favicon: "./src/images/shell.png"
+    })
+  ]
+ };
